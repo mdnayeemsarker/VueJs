95cc8610c0e0965bcfb76b652b726be322887bbb
mdnayeemsarker <nayeemasha703@gmail.com>
2024-01-03 17:31:35 +0600

    data binding to class or input value

--- 03_class/app.js
+++ 03_class/app.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app', 
     data:{
         address: 'https://facebook.com',
-        classes: 'https://facebook.com',
+        classes: ['one', 'two', 'three'],
+        input: 'My Name nayeem',
     },
 })
