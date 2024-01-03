ebb62642a4d0b569dd267ef60db3b38bee6e0945
mdnayeemsarker <nayeemasha703@gmail.com>
2024-01-04 02:05:30 +0600

    for loop

---
+++ 10_class/app.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app', 
+    data:{
+       names: ['nayeem', 'line', 'eshrat', 'asha'],
+       users: [
+        {name: 'Asha', sex: 'M', age: 24, address: 'Karani para'},
+        {name: 'Eshrat', sex: 'F', age: 27, address: 'Guzi para'},
+        {name: 'Lina', sex: 'F', age: 28, address: 'Rangpur'},
+        {name: 'Nayeem', sex: 'M', age: 24, address: 'Textile More'},
+        {name: 'Bizli', sex: 'F', age: 24, address: 'Karanipara Chourasta'},
+       ],
+    },
+})
