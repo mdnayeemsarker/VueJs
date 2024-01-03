5116e7d9f66562f2d119b438dd63edbd222f4fc8
mdnayeemsarker <nayeemasha703@gmail.com>
2024-01-04 00:22:08 +0600

    mousemove event in console log

--- 05_class/app.js
+++ 05_class/app.js
@@ -1,11 +1,18 @@
 var app = new Vue({
     el: '#app', 
     data:{
-       
+       log: {
+        x: 0,
+        y: 0,
+       }
     },
     methods: {
         Event(e){
             console.log(e);
+        },
+        over(event){
+            this.log.x = event.offsetX,
+            this.log.y = event.offsetY
         }
     },
 })
