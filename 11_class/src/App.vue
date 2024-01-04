123a61d2799c1298c9e8d36fd5dfb1e649dacdbe
mdnayeemsarker <nayeemasha703@gmail.com>
2024-01-05 01:23:09 +0600

    vuejs axios get request 19

--- 11_class/src/App.vue
+++ 11_class/src/App.vue
@@ -1,52 +1,27 @@
 <template>
   <div>
     <NavBar/>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <h3>{{ title }}</h3>
-    <AllFriends :friends="friends" @delete="deleteFriend"/>
-    <OnlineFriends :friends="friends"/>
-    <OfflineFriends :friends="friends"/>
-    <HookPart/>
+    <AllPosts/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import NavBar from './components/NavBar.vue'
-import AllFriends from './components/AllFriends.vue'
-import OnlineFriends from './components/OnlineFriends.vue'
-import OfflineFriends from './components/OfflineFriends.vue'
-import HookPart from './components/HookPart.vue'
+import AllPosts from './components/AllPosts.vue'
 
 export default {
   name: 'App',
   data(){
     return {
       title: 'Learn by MD NAYEEM SARKER',
-      friends: [
-        {name: 'A',status: true},
-        {name: 'B',status: false},
-        {name: 'C',status: true},
-        {name: 'D',status: false},
-        {name: 'E',status: true}
-      ]
     }
   },
   components: {
-    HelloWorld,
     NavBar,
-    AllFriends,
-    OnlineFriends,
-    OfflineFriends,
-    HookPart,
+    AllPosts,
   },
   methods:{
-    deleteFriend(payload){
-      // console.log(payload);
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name
-      })
-    }
   }
 }
 </script> 
---
+++ 11_class/src/components/AllPosts.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="allposts">
+        <h2>All Posts</h2> <hr>
+        <div v-for="(post, index) in posts" :key="index">
+            <h5>Post id: {{ post.id }} | Post userid: {{ post.userId }}</h5>
+            <h4>Post title: {{ post.title }}</h4>
+            <span>Post body: {{ post.body }}</span> <hr>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'AllPosts',
+    data() {
+        return {
+            posts: []
+        }
+    },
+    created(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            console.log(response.data);
+            this.posts = response.data
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+}
+</script>
+
+<style scoped>
+#h6 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #0180ff;
+}
+</style>
